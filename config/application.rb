ca4687b77f09790729ab4c3d50cba9cf3bb87953
Al Snow <jasnow@hotmail.com>
2023-09-20 15:05:08 -0400

    Upgraded gem(s): [actioncable,actionmailbox,actionmailer,actionpack,actiontext,actionview,activejob,activemodel,activerecord,activestorage,activesupport,base64,bigdecimal,config.active_record.legacy_connection_handling,connection_pool,drb,erubi,io-console,irb,method_source,mini_mime,mutex_m,net-smtp,nokogiri,rack,rack-session,rackup,rails,rails-dom-testing,rails-html-sanitizer,railties,rdoc,reline,ruby2_keywords,thor,timeout,webrick,zeitwerk]

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,6 @@ module HelloApp
 
     config.generators.javascript_engine = :js
 
-    config.active_record.legacy_connection_handling = false
+    config.active_support.cache_format_version = 7.0
   end
 end
